c4cd63d7ac011bb0070bda8537a7a9f9056d9054
Paul Brennan <brennanpincardiff@gmail.com>
2017-10-19 18:05:43 +0100

    adding travis

--- tests/automatic_checking.R
+++ tests/automatic_checking.R
@@ -1,6 +1,16 @@
 # Automated checking
 # http://r-pkgs.had.co.nz/check.html
 
+# running checks 20170819
+devtools::check()
+# creating vignettes seems to take a long time.
+# maybe do these with internal data not web!!
+# Status: 1 ERROR, 5 WARNINGs, 4 NOTEs
+# ERROR IS THIS FILE - MOVE!!
+
+
+
+
 # running checks 201708181636
 devtools::check()
 # R CMD check results
